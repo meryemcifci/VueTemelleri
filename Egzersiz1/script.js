9c2f93f5c2a1ec3bb5bf018a540fe28f0431a476
meryemcifci <meryemcifcii06@gmail.com>
2024-06-11 00:55:17 +0300

    Egzersiz1

---
+++ Egzersiz1/script.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        name:'Meryem ÇİFCİ',
+        department: 'Jr. Frontend Developer',
+        imageUrl:'https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?q=80&w=1055&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+     };
+    },
+    methods: {},
+    
+  });
+  
+  app.mount('#frontend');
+  
